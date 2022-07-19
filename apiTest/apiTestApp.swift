804d1e59e7dd2a2d369effdaa894ae21dbe6bbd7
Abe Chen <abe1272001@gmail.com>
2022-07-19 09:14:46 +0800

    Initial Commit

---
+++ apiTest/apiTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  apiTestApp.swift
+//  apiTest
+//
+//  Created by abe chen on 2022/7/19.
+//
+
+import SwiftUI
+
+@main
+struct apiTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
